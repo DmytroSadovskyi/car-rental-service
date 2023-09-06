c34f021fe7cf9c94c65b544572ef02d8fcdde50f
DmytroSadovskyi <dmytro.sadovskyi@gmail.com>
2023-09-06 17:31:21 +0300

    Create carsApi.js

---
+++ src/services/carsApi.js
@@ -0,0 +1,9 @@
+import axios from 'axios';
+axios.defaults.baseURL = 'https://645a8c7c65bd868e931e4c89.mockapi.io';
+
+const fetchCars = async () => {
+  const response = await axios.get('/cars');
+  return response.data;
+};
+
+export default fetchCars;
