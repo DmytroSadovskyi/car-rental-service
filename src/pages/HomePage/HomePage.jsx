49e2754cdbdcba25565c8685b9cac1a10601ec00
DmytroSadovskyi <dmytro.sadovskyi@gmail.com>
2023-09-07 13:47:31 +0300

    add favicon

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -2,7 +2,13 @@ const HomePage = () => {
   return (
     <>
       <h1>DreamCar rental</h1>
-      <p>Welcome to DreamCar rental</p>
+      <p>
+        Discover the Ultimate Freedom of Car Rental! Looking for a hassle-free
+        way to explore new destinations? Look no further. Welcome to DreamCar
+        rental, your gateway to unforgettable journeys. Our fleet of top-quality
+        vehicles awaits you, offering comfort, style, and the freedom to travel
+        at your own pace.
+      </p>
     </>
   );
 };
