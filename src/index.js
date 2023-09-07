d31c0921d9a6804b1ad97b850c8c92d2ffb470c5
DmytroSadovskyi <dmytro.sadovskyi@gmail.com>
2023-09-07 13:22:36 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,12 +4,19 @@ import './index.css';
 import App from './components/App';
 import 'modern-normalize/modern-normalize.css';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { store, persistor } from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter basename="/car-rental-service">
-      <App />
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <App />
+        </PersistGate>
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>
 );
