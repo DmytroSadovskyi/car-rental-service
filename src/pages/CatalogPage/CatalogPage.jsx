813cc46775bd24a63662308bb2e61aac8873a074
DmytroSadovskyi <dmytro.sadovskyi@gmail.com>
2023-09-08 23:37:18 +0300

    Update CatalogPage.jsx

--- src/pages/CatalogPage/CatalogPage.jsx
+++ src/pages/CatalogPage/CatalogPage.jsx
@@ -4,12 +4,13 @@ import axios from 'axios';
 
 import fetchCars from 'services/carsApi';
 import { CarsList, LoadMoreBtn } from './CatalogPage.styled';
+import { Container } from 'components/SharedLayout/SharedLayout.styled';
 
 const CatalogPage = () => {
   const [cars, setCars] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
   const [carsPerPage] = useState(8);
-  const [isloading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     const controller = new AbortController();
@@ -17,7 +18,6 @@ const CatalogPage = () => {
       try {
         setIsLoading(true);
         const result = await fetchCars(controller);
-        console.log(result);
         setCars(result);
       } catch (error) {
         if (axios.isCancel(error)) {
@@ -41,20 +41,21 @@ const CatalogPage = () => {
   const currentCars = cars.slice(0, indexOfLastCar);
   const handleLoadMore = () => setCurrentPage(prevPage => prevPage + 1);
   return (
-    <>
-      <p>Cars catalog</p>
-      {isloading && <p>Loading cars...</p>}
-      <CarsList>
-        {currentCars.map(car => (
-          <li key={car.id}>{<CarCard car={car} />}</li>
-        ))}
-      </CarsList>
-      {cars.length > indexOfLastCar && (
-        <LoadMoreBtn type="button" onClick={handleLoadMore}>
-          Load more
-        </LoadMoreBtn>
-      )}
-    </>
+    <section>
+      {isLoading && <p>Loading cars...</p>}
+      <Container>
+        <CarsList>
+          {currentCars.map(car => (
+            <li key={car.id}>{<CarCard car={car} />}</li>
+          ))}
+        </CarsList>
+        {cars.length > indexOfLastCar && (
+          <LoadMoreBtn type="button" onClick={handleLoadMore}>
+            Load more
+          </LoadMoreBtn>
+        )}
+      </Container>
+    </section>
   );
 };
 
