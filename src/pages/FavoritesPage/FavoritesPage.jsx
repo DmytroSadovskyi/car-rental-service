ba7c535097eb1dcfba61e0f3c6c64e8f55ceb1f1
DmytroSadovskyi <dmytro.sadovskyi@gmail.com>
2023-09-08 23:37:22 +0300

    Update FavoritesPage.jsx

--- src/pages/FavoritesPage/FavoritesPage.jsx
+++ src/pages/FavoritesPage/FavoritesPage.jsx
@@ -1,16 +1,20 @@
 import CarCard from 'components/CarCard/CarCard';
 import { useSelector } from 'react-redux';
 import { CarsList } from 'pages/CatalogPage/CatalogPage.styled';
+import { Container } from 'components/SharedLayout/SharedLayout.styled';
 
 const FavoritesPage = () => {
   const favoriteCars = useSelector(state => state.favorites);
-  console.log(favoriteCars);
   return (
-    <CarsList>
-      {favoriteCars.map(car => (
-        <li key={car.id}>{<CarCard car={car} />}</li>
-      ))}
-    </CarsList>
+    <section>
+      <Container>
+        <CarsList>
+          {favoriteCars.map(car => (
+            <li key={car.id}>{<CarCard car={car} />}</li>
+          ))}
+        </CarsList>
+      </Container>
+    </section>
   );
 };
 
