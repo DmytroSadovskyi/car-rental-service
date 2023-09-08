ef130d3e69453bc0bcc6d58158e7f801c533cfa1
DmytroSadovskyi <dmytro.sadovskyi@gmail.com>
2023-09-08 23:36:49 +0300

    Create ReviewsSlider.styled.jsx

---
+++ src/components/ReviewsSlider/ReviewsSlider.styled.jsx
@@ -0,0 +1,120 @@
+import { styled } from 'styled-components';
+import Slider from 'react-slick';
+import { ReactComponent as ArrowLeft } from '../../images/icons/down-arrow-left.svg';
+import { ReactComponent as ArrowRight } from '../../images/icons/down-arrow-right.svg';
+
+export const StyledSlider = styled(Slider)`
+  display: flex;
+  width: 100%;
+  justify-content: center;
+`;
+
+export const ReviewsWrapper = styled.div`
+  padding: 0 20px 0px 20px;
+
+  display: flex;
+  flex-direction: column;
+
+  background-color: #fff;
+
+  @media screen and (min-width: 768px) {
+    padding: 0 50px 0px 50px;
+  }
+
+  @media screen and (min-width: 1440px) {
+    padding: 0 50px 0px 50px;
+  }
+`;
+
+export const ReviewWrapper = styled.div`
+  padding: 32px;
+
+  display: flex;
+  gap: 18px;
+
+  height: 120px;
+
+  border-radius: 8px;
+  border: 1px solid rgba(17, 17, 17, 0.1);
+
+  /* @media screen and (min-width: 768px) {
+    height: 100px;
+  } */
+
+  @media screen and (min-width: 1440px) {
+    margin-right: 24px;
+  }
+`;
+
+export const Name = styled.h3`
+  margin-bottom: 13px;
+  font-size: 18px;
+  font-weight: 700;
+  line-height: 1;
+  color: #343434;
+`;
+
+export const Review = styled.p`
+  margin-top: 24px;
+  font-size: 14px;
+  font-weight: 500;
+  line-height: 1.28;
+
+  color: rgba(17, 17, 17, 0.7);
+`;
+
+export const Img = styled.img`
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+`;
+
+export const BtnArrowNext = styled.button`
+  background-color: transparent;
+  border: none;
+  position: absolute;
+  bottom: -60px;
+  left: calc(50% + 5px);
+
+  @media screen and (min-width: 768px) {
+    bottom: -80px;
+  }
+`;
+
+export const BtnArrowPrev = styled.button`
+  background-color: transparent;
+  border: none;
+  position: absolute;
+  bottom: -60px;
+  left: calc(50% - 80px);
+
+  @media screen and (min-width: 768px) {
+    bottom: -80px;
+  }
+`;
+
+export const IconLeft = styled(ArrowLeft)`
+  width: 50px;
+  height: 50px;
+
+  stroke: #111;
+  fill: transparent;
+
+  @media screen and (min-width: 768px) {
+    width: 61px;
+    height: 61px;
+  }
+`;
+
+export const IconRight = styled(ArrowRight)`
+  width: 50px;
+  height: 50px;
+
+  stroke: #111;
+  fill: transparent;
+
+  @media screen and (min-width: 768px) {
+    width: 61px;
+    height: 61px;
+  }
+`;
