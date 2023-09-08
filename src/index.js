547fb1f766d2138c43534bcdd740366cb6077caf
DmytroSadovskyi <dmytro.sadovskyi@gmail.com>
2023-09-08 23:37:13 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,14 +7,20 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store, persistor } from './redux/store';
 import { PersistGate } from 'redux-persist/integration/react';
+import { StyleSheetManager } from 'styled-components';
 
+// Define a function to filter props
+const shouldForwardProp = prop =>
+  !['currentSlide', 'slideCount'].includes(prop);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <BrowserRouter basename="/car-rental-service">
-          <App />
+          <StyleSheetManager shouldForwardProp={shouldForwardProp}>
+            <App />
+          </StyleSheetManager>
         </BrowserRouter>
       </PersistGate>
     </Provider>
