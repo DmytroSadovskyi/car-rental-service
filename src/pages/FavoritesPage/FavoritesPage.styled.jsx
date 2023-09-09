6bef281e7f86d7ebd0474c3463bf415b8d1a1f16
DmytroSadovskyi <dmytro.sadovskyi@gmail.com>
2023-09-09 21:08:37 +0300

    Update FavoritesPage.styled.jsx

---
+++ src/pages/FavoritesPage/FavoritesPage.styled.jsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const Text = styled.p`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  font-family: Inter;
+  font-size: 20px;
+  min-width: 280px;
+  text-align: center;
+`;
